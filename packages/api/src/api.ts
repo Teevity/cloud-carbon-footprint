cd4bac6105917304975ee45315a7e80deef758ab
Anthony Alcouffe <anthony.alcouffe@teevity.com>
2024-03-19 11:19:19 +0100

    Teevity/Dev - Add new endpoint to be able to populate the databse

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -8,12 +8,113 @@ import {
   FootprintApiMiddleware,
   EmissionsApiMiddleware,
   RecommendationsApiMiddleware,
+  InitFootprintApiMiddleware,
 } from './middleware'
 
 export const createRouter = (config?: CCFConfig) => {
   setConfig(config)
 
   const router = express.Router()
+
+  /**
+   * @openapi
+   * /api/initfootprint:
+   *  get:
+   *     tags:
+   *     - Footprint
+   *     summary: Gets calculated energy and carbon estimates for a given date range
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *      - name: start
+   *        in: query
+   *        description: The start date for the footprint; e.g. 2022-10-18
+   *        schema:
+   *          type: string
+   *        required: true
+   *      - name: end
+   *        in: query
+   *        schema:
+   *          type: string
+   *        description: The end date for the footprint
+   *        required: true
+   *      - name: ignoreCache
+   *        in: query
+   *        schema:
+   *          type: boolean
+   *          default: false
+   *        required: false
+   *      - name: groupBy
+   *        in: query
+   *        schema:
+   *          type: string
+   *          default: day
+   *        required: false
+   *      - name: limit
+   *        in: query
+   *        schema:
+   *          type: number
+   *          default: 50000
+   *        description: The maximum number of estimates to return (MongoDB only, ignoreCache=false)
+   *        required: false
+   *      - name: skip
+   *        in: query
+   *        schema:
+   *          type: number
+   *          default: 0
+   *        description: The maximum number of estimates to skip over (MongoDB only, ignoreCache=false)
+   *        required: false
+   *      - name: cloudProviders
+   *        in: query
+   *        schema:
+   *          type: array
+   *          items:
+   *            type: string
+   *        description: List of Cloud Providers to include in estimates (MongoDB only, Filter)
+   *        required: false
+   *      - name: accounts
+   *        in: query
+   *        schema:
+   *          type: array
+   *          items:
+   *            type: string
+   *        description: List of accounts to include in estimates (MongoDB only, Filter)
+   *        required: false
+   *      - name: services
+   *        in: query
+   *        schema:
+   *          type: array
+   *          items:
+   *            type: string
+   *        description: List of services to include in estimates (MongoDB only, Filter)
+   *        required: false
+   *      - name: regions
+   *        in: query
+   *        schema:
+   *          type: array
+   *          items:
+   *            type: string
+   *        description: List of regions to include in estimates (MongoDB only, Filter)
+   *        required: false
+   *      - name: tags
+   *        in: query
+   *        schema:
+   *          type: object
+   *          additionalProperties:
+   *            type: string
+   *        description: List of resource tags to include in estimates (MongoDB only, Filter)
+   *        required: false
+   *     responses:
+   *       200:
+   *         description: Success
+   *       400:
+   *         description: Bad request
+   *       416:
+   *         description: Partial Data Error
+   *       500:
+   *         description: Internal Server Error
+   */
+  router.get('/initfootprint', InitFootprintApiMiddleware)
 
   /**
    * @openapi
